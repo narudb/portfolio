3ec85ae04b407288bc69fc08f3dce5f8d4ccc39e
narudb <benjam.d@gmail.com>
2020-07-22 23:49:54 +0200

    Add database schema

--- back/database/schema.sql
+++ back/database/schema.sql
@@ -24,114 +24,57 @@ use your_database_name;
 -- Table structure for table `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `client_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_client_1_idx` (`project_id`),
-  CONSTRAINT `fk_client_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `client`
---
-
-LOCK TABLES `client` WRITE;
-/*!40000 ALTER TABLE `client` DISABLE KEYS */;
-/*!40000 ALTER TABLE `client` ENABLE KEYS */;
-UNLOCK TABLES;
+  PRIMARY KEY (`id`)
+);
 
 --
 -- Table structure for table `project`
 --
 
-DROP TABLE IF EXISTS `project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `project` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+	`project_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `project_description` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `project_link` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
   `project_update` datetime DEFAULT CURRENT_TIMESTAMP,
   `project_date` datetime DEFAULT NULL,
   `project_img` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `project`
---
-
-LOCK TABLES `project` WRITE;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
-UNLOCK TABLES;
+  `client_id` int NOT NULL,
+  `technologie_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_round_client1_idx` (`client_id`),
+  KEY `fk_round_technologie1_idx` (`technologie_id`),
+  CONSTRAINT `fk_round_client1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  CONSTRAINT `fk_round_technologie1` FOREIGN KEY (`technologie_id`) REFERENCES `technologie` (`id`)
+);
 
 --
 -- Table structure for table `technologie`
 --
 
-DROP TABLE IF EXISTS `technologie`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `technologie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `technologie_name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_technologie_1_idx` (`project_id`),
-  CONSTRAINT `fk_technologie_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `technologie`
---
-
-LOCK TABLES `technologie` WRITE;
-/*!40000 ALTER TABLE `technologie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `technologie` ENABLE KEYS */;
-UNLOCK TABLES;
+  `technologie_image` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_mail` varchar(96) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_password` varchar(192) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `user`
---
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-22 16:45:16
+

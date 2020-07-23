efef3c31e1dbd3bfb6ba57d0995f6b4ac20f82f0
narudb <benjam.d@gmail.com>
2020-07-23 10:37:10 +0200

    create post a new project

--- back/controllers/projects.js
+++ back/controllers/projects.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const getAllProjects = async (req, res) => {
   try {
-    let sqlRequest = `SELECT project.id AS project_id, project_title, SUBSTR(project_description, 1, 100),  project_img, client_name, technologie_name, picture_location FROM project JOIN client ON client.id=project.client_id JOIN technologie ON technologie.id=technologie_id JOIN picture ON project_id = project.id`;
+    let sqlRequest = `SELECT project.id AS project_id, project_title, SUBSTR(project_description, 1, 100) AS projectDescription,  project_img, client_name, technologie_name FROM project JOIN client ON client.id=project.client_id JOIN technologie ON technologie.id=technologie_id JOIN picture ON project_id = project.id`;
 
     const [data] = await connection.query(sqlRequest, [req.body]);
     return res.status(200).send(data);
@@ -17,11 +17,11 @@ const getAllProjects = async (req, res) => {
 const getOneProject = async (req, res) => {
   const { id } = req.params;
   try {
-    // get one Project
+    // get one project
     const [
       data,
     ] = await connection.query(
-      'SELECT project.id AS project_id, project_title, DATE_FORMAT(project_date, " %W, %d %M %Y" ) AS projectDate, project_update, client_name, project_link, picture_location, technologie_name FROM project JOIN client ON client.id=project.client_id JOIN technologie ON technologie.id=technologie_id JOIN picture ON project_id = project.id WHERE project.id = ?',
+      'SELECT project.id AS project_id, project_title, SUBSTR(project_description, 1, 100) AS projectDescription, DATE_FORMAT(project_date, " %W, %d %M %Y" ) AS projectDate, project_update, client_name, project_link, picture_one, picture_two, picture_three, picture_four, technologie_name FROM project JOIN client ON client.id=project.client_id JOIN technologie ON technologie.id=technologie_id JOIN picture ON project_id = project.id WHERE project.id = ?',
       [id]
     );
 
@@ -34,21 +34,23 @@ const getOneProject = async (req, res) => {
 
 const postProject = async (req, res) => {
   const formdata = req.body;
-  //post one news
+  //post one project
   try {
     await connection.query('INSERT INTO project SET ?', formdata);
 
-    const news = {
-      title: req.body.title,
-      author: req.body.author,
-      content: req.body.content,
-      creationDate: req.body.creation_date,
-      pictureUrl: req.body.picture_url,
+    const project = {
+      project_title: req.body.project_title,
+      project_description: req.body.project_description,
+      project_link: req.body.project_link,
+      project_date: req.body.project_date,
+      project_img: req.body.project_img,
+      client_id: req.body.client_id,
+      technologie_id: req.body.technologie_id,
     };
-    return res.status(200).send({ news });
+    return res.status(200).send({ project });
   } catch (e) {
     console.log(e);
-    return res.status(500).send('Error while reading the news.');
+    return res.status(500).send('Error while reading the projects.');
   }
 };
 

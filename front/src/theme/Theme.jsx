5535002b036515ea2d54661d40494f0d68d7a904
narudb <benjam.d@gmail.com>
2020-07-23 15:49:25 +0200

    mise en place du theme global

---
+++ front/src/theme/Theme.jsx
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    bckground: '#F9F9F9',
+    primary: '#323232',
+    hover: '#a4a4a4',
+    active: '#6c6c6c',
+  },
+  fonts: {
+    regular: 'Lato, sans-serif',
+    bold: '',
+    medium: '',
+  },
+};

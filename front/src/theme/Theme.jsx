29d4c3419f3e952c1fbc3994c4aeabaad75cfdc3
narudb <benjam.d@gmail.com>
2020-07-24 16:06:27 +0200

    modif theme

--- front/src/theme/Theme.jsx
+++ front/src/theme/Theme.jsx
@@ -1,9 +1,12 @@
 export default {
   colors: {
-    bckground: '#F9F9F9',
+    bckground: '#E4E4E4',
+    background2: '#FFFEF9',
     primary: '#323232',
     hover: '#a4a4a4',
     active: '#6c6c6c',
+    projectCard: '#EDEDED',
+    projectCardCont: '#F9F9F9',
   },
   fonts: {
     regular: 'Lato, sans-serif',

7d3f5a1a9db21151f003d43be30ed7316c7b8a2d
narudb <benjam.d@gmail.com>
2020-07-24 16:04:52 +0200

    test login page

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+// import React, { useState } from 'react';
+// import styled from 'styled-components';
+// import SignIn from './components/SignIn';
+// import SignUp from './components/SignUp';
 
-const LoginPage = () => {
-  return <div>Login Page</div>;
-};
+// const FormWrapper = styled.div`
+//   height: 45vh;
+//   grid-area: asideTop;
+//   display: grid;
+//   padding: 17px 10px;
+// `;
 
-export default LoginPage;
+// const LoginPage = () => {
+//   const [showSignup, toggleForm] = useState(true);
+//   return (
+//     <FormWrapper>
+//       {showSignup ? (
+//         <>
+//           <SignUp />
+//           <button>
+//             type="button" onClick=
+//             {() => {
+//               toggleForm(false);
+//             }}
+//             > J&apos;ai déjà un compte !
+//           </button>
+//         </>
+//       ) : (
+//         <>
+//           <SignIn />
+//           <button
+//             type="button"
+//             onClick={() => {
+//               toggleForm(true);
+//             }}
+//           >
+//             Je peux m&apos;inscrire ?
+//           </button>
+//         </>
+//       )}
+//     </FormWrapper>
+//   );
+// };
+
+// export default LoginPage;

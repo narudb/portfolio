73ea266117a0162c2d44bd18eee2d3f9de3cf240
narudb <benjam.d@gmail.com>
2020-07-24 16:07:14 +0200

    create signin and signup pages

---
+++ front/src/components/SignIn.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import styled from 'styled-components';
+import { backend } from '../conf';
+
+const SignInWrapper = styled.div`
+  height: 90%;
+`;
+
+const SignIn = () => {
+  const dispatch = useDispatch();
+  const [authUser, setAuthUser] = useState({});
+
+  const handleChange = (e) => {
+    const tmp = { ...authUser, [e.target.name]: e.target.value };
+    setAuthUser(tmp);
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    axios.post(`${backend}/auth/signin`, authUser).then(({ data }) => {
+      dispatch({
+        type: 'LOGIN',
+        payload: {
+          token: data.token,
+          mail: data.user.mail,
+          id: data.user.id,
+        },
+      });
+    });
+  };
+
+  return (
+    <SignInWrapper>
+      <Title>Je me connecte</Title>
+      <Form
+        onSubmit={(e) => {
+          signIn(e);
+        }}
+      >
+        <Input
+          type="email"
+          name="mail"
+          required
+          placeholder="Ton Email"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          required
+          placeholder="Ton mot de passe"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <InputBtn type="submit" value="Je me connecte" />
+      </Form>
+    </SignInWrapper>
+  );
+};
+
+export default SignIn;
